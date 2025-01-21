edac13fa33f94ed7078e651ada6e92f89bb6de0c
vaishnavi-ambadkar <vaishnaviambadkar183@gmail.com>
2025-01-21 13:39:48 +0530

    Update Login.jsx

--- client/src/Login.jsx
+++ client/src/Login.jsx
@@ -9,6 +9,7 @@ function Login() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const navigate = useNavigate();
+    axios.defaults.withCredentials = true;
 
     const handleSubmit = (e) => {
         e.preventDefault();
